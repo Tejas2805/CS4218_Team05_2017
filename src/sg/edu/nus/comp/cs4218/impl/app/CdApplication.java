3fc6304b65c01596ed9bbabde2973e872c3496af
tangwr <firebolt2000@hotmail.com>
2017-02-11 12:05:35 +0800

    pwd & cd

---
+++ src/sg/edu/nus/comp/cs4218/impl/app/CdApplication.java
@@ -0,0 +1,19 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import sg.edu.nus.comp.cs4218.Application;
+import sg.edu.nus.comp.cs4218.Environment;
+import sg.edu.nus.comp.cs4218.exception.CdException;
+
+public class CdApplication implements Application{
+	
+	@Override
+	public void run(String[] args, InputStream stdin, OutputStream stdout) 
+			throws CdException {
+
+	}
+}
---
+++ src/sg/edu/nus/comp/cs4218/impl/app/PwdApplication.java
@@ -0,0 +1,32 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import sg.edu.nus.comp.cs4218.Application;
+import sg.edu.nus.comp.cs4218.Environment;
+import sg.edu.nus.comp.cs4218.exception.PwdException;
+
+public class PwdApplication implements Application{
+	
+	@Override
+	public void run(String[] args, InputStream stdin, OutputStream stdout) 
+			throws PwdException {
+		String currDir = "";
+		
+		if (args.length == 0) {
+			currDir = Environment.currentDirectory + System.lineSeparator();
+			
+		    try {
+		    	
+                stdout.write(currDir.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();    
+            }
+		} else {
+			throw new PwdException("Invalid Arguments");
+		}
+	}
+}
