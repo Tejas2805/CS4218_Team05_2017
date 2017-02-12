61780b791f816d7f6d058ff74eef40bc4a1c9399
kohchenggee <kohchenggee@gmail.com>
2017-02-12 16:33:30 +0800

    Add HeadApplication but haven't refactor

--- src/sg/edu/nus/comp/cs4218/impl/app/HeadApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/HeadApplication.java
@@ -1,17 +1,109 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import sg.edu.nus.comp.cs4218.Application;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.HeadException;
 
 public class HeadApplication implements Application {
 
+	private static final String NULL_ALL = "args, stdin, stdout are null";
+	
+	private static final int DEFAULT_LINE_COUNT = 10;
+	
+	private static final String NEWLINE = System.getProperty("line.separator");
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
+		if(checkNullInput(args,stdin,stdout)){
+			throw new HeadException(NULL_ALL);
+		}
+		if(args.length==0){
+			try{
+				for(int i=0;i< DEFAULT_LINE_COUNT;i++){
+					int outputMsg;
+					while((outputMsg = stdin.read()) != -1){
+						stdout.write(outputMsg);
+					}
+				}
+				
+			}catch (IOException io) {
+				throw new HeadException(io.getMessage());
+			}catch (Exception e){
+				throw new HeadException(e.getMessage());
+		}
 		// TODO Auto-generated method stub
 		
+	}
+		if(args.length==1){
+			try {
+				Path path = Paths.get(args[0]);
+				BufferedReader br = Files.newBufferedReader(path);
+				String line;
+				for (int i = 0; i < DEFAULT_LINE_COUNT; i++) {
+					if ((line = br.readLine()) == null) {
+						break;
+					}
+					stdout.write(line.getBytes());
+					stdout.write(NEWLINE.getBytes());
+				}
+				
+			} catch (IOException ioe) {
+				throw new HeadException(ioe.getMessage());
+		}
+		}
+		if(args.length==2){
+			try{
+				int lineCount = Integer.parseInt(args[1]);
+				if(lineCount<=0){
+					throw new HeadException("Invalid Line Count");
+				}
+				for(int i=0;i< lineCount;i++){
+					int outputMsg;
+					while((outputMsg = stdin.read()) != -1){
+						stdout.write(outputMsg);
+					}
+				}
+			}catch (IOException io) {
+				throw new HeadException(io.getMessage());
+			}catch (Exception e){
+				throw new HeadException(e.getMessage());
+		}
+			}
+		if(args.length==3){
+			try{
+				int lineCount = Integer.parseInt(args[1]);
+				if(lineCount<=0){
+					throw new HeadException("Invalid Line Count");
+				}
+				Path path = Paths.get(args[0]);
+				BufferedReader br = Files.newBufferedReader(path);
+				String line;
+				for (int i = 0; i < DEFAULT_LINE_COUNT; i++) {
+					if ((line = br.readLine()) == null) {
+						break;
+					}
+					stdout.write(line.getBytes());
+					stdout.write(NEWLINE.getBytes());
+				}
+			}catch (IOException ioe) {
+				throw new HeadException(ioe.getMessage());
+		}
+		}
+		
+	}
+			
+
+	private boolean checkNullInput(String[] args, InputStream stdin, OutputStream stdout) {
+		// TODO Auto-generated method stub
+		return (args==null) && (stdin==null) && (stdout==null);
 	}
 
 }
