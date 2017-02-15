e92552b3ed40820914ebf1c914624cad38a5f823
wireless90 <wireless-@hotmail.com>
2017-02-15 21:59:42 +0800

    Grep done. Pray to god it works. No testing..

--- src/sg/edu/nus/comp/cs4218/impl/app/GrepApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/GrepApplication.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.*;
@@ -23,44 +25,43 @@ public class GrepApplication implements Grep {
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
 		
-		pattern = Pattern.compile(args[0]);
 		
 		if(stdout == null)
 			throw new GrepException("OutputStream not provided");
+		
+
+		String newArg = "";
+		String output;
+		
+		for(String arg : args){ newArg += arg + "\n";}
 		
 		if (args == null || args.length == 0) {
 			if (stdin == null) {
 				throw new GrepException("InputStream not provided");
 			} else {
 				//TODO
-				//return grepFromStdin(); 
+				output = grepFromStdin(newArg); 
 			}
 		}else {
 			int numOfFiles = args.length - 1;
 
-			if (numOfFiles > 0) {
-				Path filePath;
-				Path[] filePathArray = new Path[numOfFiles];
-				Path currentDir = Paths.get(Environment.currentDirectory);
-				boolean isFileReadable = false;
-
-				for (int i = 1; i < numOfFiles; i++) {
-					filePath = currentDir.resolve(args[i]);
-					isFileReadable = checkIfFileIsReadable(filePath);
-					if (isFileReadable) {
-						filePathArray[i] = filePath;
-					}
-				}
-
-				// file could be read. perform grep command
-				if (filePathArray.length == 1) {
-					grepFromOneFile(filePathArray[0]);					
-				}
-				else if(filePathArray.length>1){
-
-					grepFromMultipleFiles(filePathArray);;
-				}
+			switch(numOfFiles){
+			case 0:
+				output = "\n";
+				break;
+			case 1:
+				output = grepFromOneFile(newArg);
+				break;
+			default:
+				output = grepFromMultipleFiles(newArg);
+				break;
 			}
+		}
+		
+		try {
+			stdout.write(output.getBytes());
+		} catch (IOException e) {
+			throw new CatException("Exception Caught");
 		}
 			
 	}
@@ -73,74 +74,201 @@ public class GrepApplication implements Grep {
 	 * @return True if the file is readable.
 	 * @throws CatException
 	 *             If the file is not readable
+	 * @throws GrepException 
 	 */
 	
-	boolean checkIfFileIsReadable(Path filePath) throws CatException {
+	boolean checkIfFileIsReadable(Path filePath) throws GrepException {
 		
 		if (Files.isDirectory(filePath)) {
-			throw new CatException("This is a directory");
+			throw new GrepException("This is a directory");
 		}
 		if (Files.exists(filePath) && Files.isReadable(filePath)) {
 			return true;
 		} else {
-			throw new CatException("Could not read file");
+			throw new GrepException("Could not read file");
 		}
 	}
+	
+	List<Path> getValidFilePathsFromString(String[] strFilePaths, int start, int end){
+		List<Path> validFilePaths = new ArrayList<>();
+		Path filePath;
 		
+		for(int i = start; i <=end; i++){
+			
+			try{
+				filePath = Paths.get(strFilePaths[i]);
+			}catch(InvalidPathException e){
+				continue;
+			}
+			
+			try {
+				if(checkIfFileIsReadable(filePath)){
+					validFilePaths.add(filePath);
+				}
+			} catch (GrepException e) {
+				continue;
+			}
+		}
+		
+		return validFilePaths;
+	}
+	
 	@Override
-	public String grepFromStdin(String filePath) {
-		List<String> linesfromFile = Files.readAllLines(filePath);
+	public String grepFromStdin(String args) {
+		if(grepInvalidPatternInFile(args) != null){
+			return "\n";
+		}
 		
+		String[] eachArg = args.split("\n");
+		String strPattern = eachArg[0];
+		String output = "";
+		
+		for(int i = 1; i < eachArg.length; i++){
+			output += performGrep(strPattern, eachArg[i]);
+		}
+		
+		return output;
 	}
 
+	/*
+	 *	Args is of format
+	 * [pattern\nfilePath] 
+	 */
 	@Override
-	public List<String> grepFromOneFile(Path filePath) {
+	public String grepFromOneFile(String args) {
+		if(grepInvalidPatternInFile(args) != null){
+			return "\n";
+		}
+		String strPattern = args.split("\n")[0];
+		String[] eachArg = args.split("\n");
+		
+		if(eachArg.length == 0 || eachArg.length > 2){
+			return "Invalid number of Arguments";
+		}
+		
+		List<Path> validFilePaths = getValidFilePathsFromString(eachArg, 1, 1);
+		
+		if(validFilePaths.size() == 0)
+			return "\n";
+		
+		return performGrep(strPattern, validFilePaths.get(0));
+	}
+
+	String performGrep(String strPattern, String line){
+		Pattern pattern;
+		pattern = Pattern.compile(strPattern);
+		String output="";
+		Matcher matcher =  pattern.matcher(line);
+		if(matcher.find())
+		{
+			if(matcher.group().length() >0)
+				return line + "\n";
+		}
+		
+		return "";
+	}
+	
+	String performGrep(String strPattern, Path filePath)
+	{
+		Pattern pattern;
+		pattern = Pattern.compile(strPattern);
 		List<String> linesInFile = new ArrayList<>();
+		String output="";
+		
 		try {
 			linesInFile = Files.readAllLines(filePath);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return "";
 		}
-		List<String> output = new ArrayList<String>();
-		Matcher matcher;
 		
+		Matcher matcher;
 		for(String line : linesInFile){
 			matcher =  pattern.matcher(line);
 			if(matcher.find())
 			{
 				if(matcher.group().length() >0)
-					output.add(line);
+					output += line +"\n";
 			}
 		}
 		
 		return output;
 	}
-
+	/*
+	 *	Args is of format
+	 * [pattern\nfilePath] 
+	 */
 	@Override
-	public List<String> grepFromMultipleFiles(Path[] filePaths) {
-		List<String> output = new ArrayList<>();
-		List<String> fileOutput;
+	public String grepFromMultipleFiles(String args) {
+		if(grepInvalidPatternInFile(args) != null){
+			return "\n";
+		}
+		String strPattern = args.split("\n")[0];
+		String[] eachArg = args.split("\n");
 		
-		for(Path filePath : filePaths){
-			fileOutput = grepFromOneFile(filePath);
-			for(String string : fileOutput){
-				output.add(filePath.toString() + ": " + string);
-			}
+		if(eachArg.length >=0 && eachArg.length <= 2){
+			return "Invalid number of Arguments";
+		}
+		
+		List<Path> validFilePaths = getValidFilePathsFromString(eachArg, 1, eachArg.length-1);
+		
+		if(validFilePaths.size() == 0)
+			return "\n";
+		
+		String output = "";
+		
+		for(Path filePath : validFilePaths){
+			output+= performGrep(strPattern, filePath);
 		}
 		return output;
 	}
 
+	/*
+	 *	Args is of format
+	 * [pattern] 
+	 */
 	@Override
 	public String grepInvalidPatternInStdin(String args) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String[] eachArg = args.split("\n");
+		 
+		if(eachArg.length == 0)
+			return "No Pattern Found";
+		 
+		 // Check if pattern
+		 Pattern p;
+		 
+		 try
+		 {
+			 p = Pattern.compile(eachArg[0]);
+		 }catch(PatternSyntaxException e){
+			 return "Invalid Pattern";
+		 }
+		 
+		 return null;//No errors
 	}
 
+	/*
+	 *	Args is of format
+	 * [pattern\nfilePath] 
+	 */
 	@Override
 	public String grepInvalidPatternInFile(String args) {
-		// TODO Auto-generated method stub
-		return null;
+		 String[] eachArg = args.split("\n");
+		 
+		 if(eachArg.length == 0)
+			 return "No Pattern Found";
+		 
+		 // Chheck if pattern is ok
+		 Pattern p;
+		 
+		 try
+		 {
+			 p = Pattern.compile(eachArg[0]);
+		 }catch(PatternSyntaxException e){
+			 return "Invalid Pattern";
+		 }
+		 
+		 return null;//No errors
 	}
 
 }
