841805c16b7faa8a365e0da8132bb1b97210fc92
tangwr <firebolt2000@hotmail.com>
2017-02-19 15:40:38 +0800

    ef2 date junit test

--- Tests/ef2/unit/TestDateApplication.java
+++ Tests/ef2/unit/TestDateApplication.java
@@ -2,26 +2,55 @@ package ef2.unit;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.TimeZone;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import sg.edu.nus.comp.cs4218.exception.DateException;
 import sg.edu.nus.comp.cs4218.impl.app.DateApplication;
 
 public class TestDateApplication {
 
 	DateApplication dataApp;
-	
+
 	@Before
 	public void setup(){
 		dataApp = new DateApplication();
 	}
-	
+
 	@Test
-	public void testOutput() {
-		dataApp.run("", null, null);
+	public void testPrintCurrentDate() throws DateException {
+		LocalDateTime now = LocalDateTime.now();
+		Calendar calendar = Calendar.getInstance();
+
+		int year = now.getYear();
+		int month = now.getMonthValue();
+		int day = now.getDayOfMonth();
+		int hour = now.getHour();
+		int minute = now.getMinute();
+		int second = now.getSecond();
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+		String[] strDays = new String[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+		String[] strMonths = new String[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+		TimeZone timeZone = TimeZone.getTimeZone("Asia/Singapore");
+		String strTimeZone = timeZone.getDisplayName(false, TimeZone.SHORT);
+		
+		String expectedResults = strDays[dayOfWeek -1] + " " + strMonths[month-1] + " " + String.format("%02d",day) + 
+	    		" " + String.format("%02d",hour) + ":" + String.format("%02d",minute) + ":" + String.format("%02d",second) + 
+	    		" " + strTimeZone + " "  + year;
+		
+		String args = "";
+		String actualResults = dataApp.printCurrentDate(args);
+		
+		assertEquals(expectedResults, actualResults);
 	}
-	
+
 	@After
 	public void tearDown(){
 		dataApp = null;
