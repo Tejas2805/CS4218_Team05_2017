bdb9b5a8b8d661fa389dd70ddd1ce9927b495cd0
wireless90 <wireless-@hotmail.com>
2017-02-22 10:53:01 +0800

    Test Redirection Done

---
+++ Tests/ef1/unit/TestRedirection.java
@@ -0,0 +1,206 @@
+package ef1.unit;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.CatException;
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+import sg.edu.nus.comp.cs4218.impl.ShellImpl;
+
+public class TestRedirection {
+
+	static ShellImpl shellImpl;
+	static ByteArrayOutputStream output;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		shellImpl = new ShellImpl();
+	}
+	
+	@Test
+	public void catInputRedirectionFromOneFile(){
+		String input = "cat ef1_test_cases\\cat\\test.txt";
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		String expected = "this is a file \"test.txt\"\n", actual;
+		
+		try {
+			shellImpl.parseAndEvaluate(input, output);
+		} catch (AbstractApplicationException e) {
+			e.printStackTrace();
+			fail();
+		} catch (ShellException e) {
+			e.printStackTrace();
+			fail();
+		}
+		actual = output.toString();
+		assertEquals(expected, actual);		
+	}
+
+	@Test
+	public void echoOutputRedirection(){
+		File f = new File("a.txt");
+		
+		if(f.exists() && !f.isDirectory()){
+			f.delete();
+		}
+				
+		String input = "echo hello > a.txt";
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		String expected = "hello\r\n", actual;
+		
+		try {
+			shellImpl.parseAndEvaluate(input, output);
+		} catch (AbstractApplicationException e) {
+			e.printStackTrace();
+			fail();
+		} catch (ShellException e) {
+			fail();
+		}
+		
+		BufferedReader br = null;
+		StringBuilder sb = null;
+		try {
+			br = new BufferedReader(new FileReader("a.txt"));
+		} catch (FileNotFoundException e) {
+			fail();
+		}
+		try {
+		    sb = new StringBuilder();
+		    String line = null;
+			try {
+				line = br.readLine();
+			} catch (IOException e) {
+				fail();
+			}
+
+		    while (line != null) {
+		        sb.append(line);
+		        sb.append(System.lineSeparator());
+		        try {
+					line = br.readLine();
+				} catch (IOException e) {
+					fail();
+				}
+		    }
+		    String everything = sb.toString();
+
+			actual = everything;
+			
+			char[] ac = actual.toCharArray();
+			char[] ex = expected.toCharArray();
+			
+			for(int i =0; i < ac.length;i++){
+				int a = ac[i];
+				int b = ex[i];
+				if(a == b)
+					System.out.println("ok");
+				else
+					System.out.println("not ok");
+			}
+			
+			assertEquals(expected, actual);
+		} finally {
+		    try {
+				br.close();
+			} catch (IOException e) {
+				fail();
+			}
+
+		}
+				
+	}
+	
+
+	@Test
+	public void catInputAndOutputRedirection(){
+
+		File f = new File("a.txt");
+		
+		if(f.exists() && !f.isDirectory()){
+			f.delete();
+		}
+			
+		
+		String input = "cat < ef1_test_cases\\cat\\test.txt > a.txt";
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		String expected = "this is a file \"test.txt\"\r\n", actual;
+		
+		try {
+			shellImpl.parseAndEvaluate(input, output);
+		} catch (AbstractApplicationException e) {
+			e.printStackTrace();
+			fail();
+		} catch (ShellException e) {
+			e.printStackTrace();
+			fail();
+		}
+		
+
+		BufferedReader br = null;
+		StringBuilder sb = null;
+		try {
+			br = new BufferedReader(new FileReader("a.txt"));
+		} catch (FileNotFoundException e) {
+			fail();
+		}
+		try {
+		    sb = new StringBuilder();
+		    String line = null;
+			try {
+				line = br.readLine();
+			} catch (IOException e) {
+				fail();
+			}
+
+		    while (line != null) {
+		        sb.append(line);
+		        sb.append(System.lineSeparator());
+		        try {
+					line = br.readLine();
+				} catch (IOException e) {
+					fail();
+				}
+		    }
+		    String everything = sb.toString();
+
+			actual = everything;
+			
+			char[] ac = actual.toCharArray();
+			char[] ex = expected.toCharArray();
+			
+			for(int i =0; i < ac.length;i++){
+				int a = ac[i];
+				int b = ex[i];
+				if(a == b)
+					System.out.println("ok");
+				else
+					System.out.println("not ok");
+			}
+			
+			assertEquals(expected, actual);
+		} finally {
+		    try {
+				br.close();
+			} catch (IOException e) {
+				fail();
+			}
+
+		}
+
+	}
+}
