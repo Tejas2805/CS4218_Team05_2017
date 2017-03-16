81430b9c7e24ab2162567b1f6a47b5189c8ebbaf
phoon0901 <phoon0901@hotmail.com>
2017-03-16 12:33:09 +0800

    pipe test

---
+++ Tests/ef2/unit/TestPipeOperator.java
@@ -0,0 +1,14 @@
+package ef2.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPipeOperator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
