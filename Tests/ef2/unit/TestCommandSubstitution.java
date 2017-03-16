7d7ee350720531f48ed78905945926fee0287498
kohchenggee <kohchenggee@gmail.com>
2017-03-16 12:55:50 +0800

    push 1 more time

--- Tests/ef2/unit/TestCommandSubstitution.java
+++ Tests/ef2/unit/TestCommandSubstitution.java
@@ -2,11 +2,12 @@ package ef2.unit;
 import static org.junit.Assert.*;
 public class TestCommandSubstitution {
 
-	static final 
+	static final String NEWLINE = System.getProperty("line.separator");
 	 @Test
 	    public void testCatWithCommandSubstitution() throws AbstractApplicationException, ShellException {
-	        shell.parseAndEvaluate("cat `echo test1.txt` test2.txt", testOut);
+		 ShellImpl shell;
+		 shell.parseAndEvaluate("cat `echo test1.txt` test2.txt", testOut);
 	        assertEquals(1,1);
-	    assert("test1" + NL + "test1test2" + NL + "test2", testOut.toString());
+	    assert("test1" + NEWLINE + "test1test2" + NEWLINE + "test2", testOut.toString());
 	    }
 }
