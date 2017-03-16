f85114b99eaac30cfaa2180cd47bad0a0aa5b7ff
phoon0901 <phoon0901@hotmail.com>
2017-03-16 12:57:10 +0800

    pipe unit test

--- Tests/ef2/unit/TestPipeOperator.java
+++ Tests/ef2/unit/TestPipeOperator.java
@@ -2,13 +2,43 @@ package ef2.unit;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+import sg.edu.nus.comp.cs4218.impl.ShellImpl;
 
 public class TestPipeOperator {
 
+	static ShellImpl shellImpl;
+	static ByteArrayOutputStream output;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		shellImpl = new ShellImpl();
+		sg.edu.nus.comp.cs4218.Environment.currentDirectory = System.getProperty("user.dir");
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEchoPipeEcho(){
+		String input = "echo test|echo test1";
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		String expected = "test1\n", actual;
+		
+		try {
+			shellImpl.parseAndEvaluate(input, output);
+		} catch (AbstractApplicationException e) {
+			// TODO Auto-generated catch block
+			fail();
+		} catch (ShellException e) {
+			// TODO Auto-generated catch block
+			fail();
+		}
+		actual = output.toString();
+		assertEquals(expected, actual);		
 	}
 
 }
