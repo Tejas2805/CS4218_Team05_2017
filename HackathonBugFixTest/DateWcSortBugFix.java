5f0a7cd503f37b9a0ca3681e41ad59c5a8edb3f3
tangwr <firebolt2000@hotmail.com>
2017-04-12 20:29:37 +0800

    rm folder

--- HackathonBugFixTest/DateWcSortBugFix.java
+++ HackathonBugFixTest/DateWcSortBugFix.java
@@ -1,7 +1,13 @@
 import static org.junit.Assert.*;
 
+import java.io.OutputStream;
+
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import sg.edu.nus.comp.cs4218.impl.ShellImpl;
+import sg.edu.nus.comp.cs4218.impl.app.WcApplication;
 
 public class DateWcSortBugFix {
 
@@ -9,8 +15,18 @@ public class DateWcSortBugFix {
 	//Wc: return nothing when using more than 1 options + stdin
 	//Sort: numbers (-n option) wit stdin returns nothing
 	//Date: run correctly and also throw exception on more than 0 input args
+	
+	ShellImpl shellImpl;
+	OutputStream stdout;
+	WcApplication wcApp;
+	
+	@Before
+    public void setUp(){
+		shellImpl = new ShellImpl();
+	}
 	@After
 	public void tearDown() throws Exception {
+		shellImpl = null;
 	}
 
 	@Test
